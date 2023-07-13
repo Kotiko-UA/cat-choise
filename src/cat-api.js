400a5735329683fd76cc126afe4e60cab50b98d8
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-07-13 15:18:20 +0300

    1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_CS9JGhlT1Lg9NL1vNlkHXdy89ch6G7SfoE9iWYoNoIY9D42Pf4PIfChAtyCwLHiF';
-
-const CAT_URL = 'https://api.thecatapi.com/v1/images/search?breed_ids=';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 export function fetchBreeds() {
-  return fetch(`https://api.thecatapi.com/v1/breeds`).then(response => {
+  return axios.get(`/breeds/`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -12,7 +11,7 @@ export function fetchBreeds() {
   });
 }
 export function fetchCatByBreed(breedId) {
-  return fetch(`${CAT_URL}${breedId}`).then(response => {
+  return axios.get(`/images/search?breed_ids=${breedId}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
-
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_CS9JGhlT1Lg9NL1vNlkHXdy89ch6G7SfoE9iWYoNoIY9D42Pf4PIfChAtyCwLHiF';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 const refs = {
   selectBreesd: document.querySelector('.breed-select'),
   loaderEl: document.querySelector('.loader'),
